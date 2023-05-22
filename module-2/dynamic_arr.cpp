6f395739012542c4418dd9aa33d5b79b13379769
probir ghosh <probirghosh@outlook.com>
2023-05-23 04:44:23 +0600

    module-2 ,  exploring dynnamic arr :tada:

---
+++ module-2/dynamic_arr.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int *arr = new int [6];
+
+    for (int i = 0; i < 6 ; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < 6 ; i++)
+    {
+        cout << arr[i] << " ";
+    }
+        
+
+    return 0;
+}
