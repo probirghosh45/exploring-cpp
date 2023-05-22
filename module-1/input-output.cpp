998987e9c0bbeaaa34716f197f83a547e9970b71
probir ghosh <probirghosh@outlook.com>
2023-05-22 08:56:35 +0600

    io c++ :tada:

---
+++ module-1/input-output.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // printf("for c");
+    std::cout << "for c++\n" ;
+    std::cout << "for c++" <<std::endl; ;
+    std::cout << "Hello " << "world" << std::endl;
+    std::cout << "Hello "<< std::endl << "world" << std::endl;
+  
+    int a=5,b=10;
+
+    // std::cout << a ;
+    // std::cout << a  << std::endl;
+    // std::cout << a  << std::endl << b ;
+    std::cout << a << " " << b << std::endl;
+    
+    
+    int m=50,n=100;    
+    cout << m  << endl << 100 ;
+    return 0;
+}
+
---
+++ module-1/tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+ // int a=5,b=10;
+
+    // std::cout << a << std::endl << std::cout << b << std::endl;
