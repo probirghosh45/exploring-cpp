e30b7e8b45bb033b4f4141575b692bef45c410e4
probir ghosh <probirghosh@outlook.com>
2023-05-22 09:46:25 +0600

    io c++ :tada:

--- module-1/input-output.cpp
+++ module-1/input-output.cpp
@@ -2,22 +2,32 @@
 using namespace std;
 int main()
 {
-    // printf("for c");
-    std::cout << "for c++\n" ;
-    std::cout << "for c++" <<std::endl; ;
-    std::cout << "Hello " << "world" << std::endl;
-    std::cout << "Hello "<< std::endl << "world" << std::endl;
-  
-    int a=5,b=10;
+
+    // input in c++ format
+    int p, q;
+    // scanf("%d %d", &p , &q); // input in c format
+    cin >> p >> q;
+    cout << p << " " << q << endl;
+    
+    // output in c++ format    
+    // printf("for c"); // output in c format
+    std::cout << "for c++\n";
+    std::cout << "for c++" << std::endl;
+    std::cout << "Hello "
+              << "world" << std::endl;
+    std::cout << "Hello " << std::endl
+              << "world" << std::endl;
+
+    int a = 5, b = 10;
 
     // std::cout << a ;
     // std::cout << a  << std::endl;
     // std::cout << a  << std::endl << b ;
     std::cout << a << " " << b << std::endl;
-    
-    
-    int m=50,n=100;    
-    cout << m  << endl << 100 ;
+
+    int m = 50, n = 100;
+    cout << m << endl
+         << n << endl;
+
     return 0;
 }
-
