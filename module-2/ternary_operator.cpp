8d5979b1a72cb92f39ad34e6570fbbe80ef8bbd2
probir ghosh <probirghosh@outlook.com>
2023-05-23 06:45:52 +0600

    module-2 , ternary operator :hammer:

---
+++ module-2/ternary_operator.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+
+    // if (a % 2 == 0)
+    // {
+    //     cout << "Even";
+    // }
+    // else {
+    //     cout << "Odd";
+    // }
+
+
+    // ternary operator
+    (a%2==0) ? cout << "Even" : cout << "Odd" ;
+
+    return 0;
+}
