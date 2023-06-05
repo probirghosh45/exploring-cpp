949429f1bfbb4b0b4e2a106453aaacb673939736
probir ghosh <probirghosh@outlook.com>
2023-06-05 19:50:17 +0600

    string input explored :tada:

---
+++ module-5 [week-2]/string_input_spaces.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int a;
+    cin >> a;
+    cin.ignore();
+    string s;
+    // cin >> s;
+    getline(cin, s);
+    cout << a << endl;
+    cout << s << endl;
+    return 0;
+}
